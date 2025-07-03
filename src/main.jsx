1a5aa1c89e34da184965ddd3b052990b4ab7d97e
gideon <adeniranakinloluwa2000@gmail.com>
2025-07-03 02:40:43 +0100

    empty pages after SEO fixed

--- src/main.jsx
+++ src/main.jsx
@@ -15,13 +15,18 @@
 // main.js
 import React from 'react';
 import App from './App';
+import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </HelmetProvider>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </StrictMode>
 );
