996b2f62f927b1277ed5941b5b7729ffdfe7a39d
gideon <adeniranakinloluwa2000@gmail.com>
2025-07-03 01:34:00 +0100

    SEO updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { HelmetProvider } from 'react-helmet-async'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <HelmetProvider>
+//       <App />
+//     </HelmetProvider>
+//   </StrictMode>
+// )
+
+// main.js
+import React from 'react';
+import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>
+);
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+// src/routes.js
+export const routes = [
+    '/', 
+  ];
+  
